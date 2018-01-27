25d661104b13d12b3c94285e4d0ec9c11e9bced8
Amanda Wehrman <arwehrman@gmail.com>
2018-01-27 19:07:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 let store = { drivers: [], passengers: [], trips: []};
+
+let driverId = 0
+
+class Driver {
+  constructor(name){
+    this.id = ++userId
+    this.name = name
+
+    // insert in the user to the store
+    store.drivers.push(this)
+  }
+}
