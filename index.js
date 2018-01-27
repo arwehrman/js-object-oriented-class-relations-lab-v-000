f3b6ebe9eb5a86e85a4470d5adedcbb9aaf4856c
Amanda Wehrman <arwehrman@gmail.com>
2018-01-27 19:26:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ class Driver {
   constructor(name){
     this.id = ++driverId
     this.name = name
+    // insert in the driver to the store
     store.drivers.push(this)
+  }
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId === this.id;
+    });
+  }
+
+  passengers() {
+    return this.trips().map(trip => {
+      return trip.passenger();
+    });
   }
 }
 
